6ea50ddab126d41eb9e3381a9241d5751131fe2c
SoulBee <kajolvarma98@gmail.com>
2024-05-07 17:20:52 +0530

    initial commit

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -161,7 +161,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_RESET);
